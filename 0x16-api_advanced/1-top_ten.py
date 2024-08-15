e20a0262cd3cb5d0329d5e41e3517d6563b4ed7b
muhdmmad <mohd33034914@gmail.com>
2024-08-15 23:18:46 +0000

    apis

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""Module for task 1"""
+"""Module for task 0"""
 
 
-def top_ten(subreddit):
-    """Queries the Reddit API and returns the top 10 hot posts
-    of the subreddit"""
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
     import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
     if sub_info.status_code >= 300:
-        print('None')
-    else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+        return 0
+
+    return sub_info.json().get("data").get("subscribers")
